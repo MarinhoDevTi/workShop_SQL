a6a46ea2150e0b1ac364019fab022f9f03c7a399
marinho <diogomarinhomg@gmail.com>
2021-09-22 20:59:17 -0300

    Update

--- insert.sql
+++ insert.sql
@@ -1,8 +1,12 @@
-INSERT INTO paciente (prontuario, nome, cpf, convenio, data_consulta, especialidade)
-VALUES (11, 'Maria', 11111111111, 'unimed', '2021-08-26', 'neurologia');
-INSERT INTO paciente (prontuario, nome, cpf, convenio, data_consulta, especialidade)
-VALUES (25, 'Lucas', 22222222222, 'unimed', '2021-09-11', 'cardiologia');
-INSERT INTO paciente (prontuario, nome, cpf, convenio, data_consulta, especialidade)
-VALUES (7, 'Rosa', 33333333333, 'sus', '2021-09-08', 'psiquiatria');
-INSERT INTO paciente (prontuario, nome, cpf, convenio, data_consulta, especialidade)
-VALUES (513, 'Antonio', 44444444444, 'sus', '2021-08-29', 'ortopedia');
+INSERT INTO hospital(cnpj, nome, endereco, cod_paciente)
+VALUES (555555, 'Santa Casa', 'Rua Sao Jose', 25);
+INSERT INTO hospital(cnpj, nome, endereco, cod_paciente)
+VALUES (365254, 'Hospital das Clinicas', 'Avenida Brasil', 513);
+
+
+SELECT * FROM paciente;
+SELECT nome FROM paciente;
+SELECT nome, data_consulta FROM paciente;
+SELECT * FROM paciente WHERE convenio='sus';
+SELECT * FROM paciente WHERE data_consulta>='2021-09-01';
+SELECT * FROM paciente,hospital WHERE paciente.prontuario=hospital.cod_paciente;
